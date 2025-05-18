fcd8a8410454abeaeb3feb537b9563e42c9a86e3
Jannatul Ferdous Oni <jannatuloni41@gmail.com>
2025-05-18 21:36:27 +0600

    Create Puzzle Hunt.cpp

---
+++ Puzzle Hunt.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+        int N;
+        cin>>N;
+
+        if(N>=6 && N<=8){
+            cout<<"YES\n";
+        }
+        else{
+            cout<<"NO\n";
+        }
+}
+
